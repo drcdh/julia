3ec6e5bf677580a694c69f397a609b9dbadaa7a0
drcdh <danielrhunter@gmail.com>
2015-12-03 14:04:41 -0500

    Implement usage and options with docopt.

--- julia.py
+++ julia.py
@@ -1,24 +1,30 @@
+"""Julia.
+
+Usage:
+    julia.py [--imgname=<filename>] [--imgsize=<px>] [--zoom=<z>] [--zlimit=<zlim>] [--center=<c_r>,<c_i>] [--cmap=<cmap>] [--soften] -- <z_r> <z_i>
+    julia.py (-h | --help)
+
+Options:
+    -h --help               Show this screen.
+    --imgname=<filename>    Pathname to write image [default: julia.png].
+    --imgsize=<px>          Image size in pixels [default: 512].
+    --zoom=<z>              Zoom on center (>1 is "in") [default: 2.0].
+    --zlimit=<zlim>         Maximum value of |z| in recusion [default: 2.0].
+    --center=<c_r>,<c_i>    Real and imaginary parts of center point [default: 0.0,0.0].
+    --cmap=<cmap>           Colormap name [default: cubehelix].
+    --soften                Soften the iterator.
+
+"""
+
+from docopt import docopt
+
 import Image
 import numpy as np
 
 from math import log
 from matplotlib.pyplot import get_cmap
 
-width = 0.5
-height = 0.5
-center = complex(0,0)
-
-xPixels = 1024
-yPixels = 1024
-
 maxIterations = 900
-zLimit = 2.0
-
-#C = complex(-0.70176, -0.3842)
-C = complex(-0.4, 0.6)
-#C = complex(-0.8, 0.156)
-
-cmap = get_cmap('cubehelix')
 
 def f(z):
     return pow(z,2) + C
@@ -36,38 +42,48 @@ def iterate(zn, znm1=None, n=0):
     return iterate(znp1, zn, n+1)
 
 def pixelCoord(i,j):
-    px = (width/2.0)*float(i-xPixels/2)/float(xPixels/2)
-    py = (height/2.0)*float(j-yPixels/2)/float(yPixels/2)
+    px = (1.0/(2.0*zoom))*float(j-imgsize/2)/float(imgsize/2)
+    py = (1.0/(2.0*zoom))*float(i-imgsize/2)/float(imgsize/2)
     pz = center + complex(px, py)
     return pz
 
-def colorize(i, norm):
+def colorize(i, norm, cmap):
     c = float(i)/float(norm)
     r, g, b, l = cmap(c)
     return [int(255*x) for x in (b, g, r)]
 
 if __name__ == '__main__':
-    global largestNorm
-    grid = np.ndarray(shape=(yPixels, xPixels), dtype=np.uint8)
+    global C, imgsize, center, zoom, zLimit
+
+    arg = docopt(__doc__, options_first=True)
+    C = complex(*map(float, [arg['<z_r>'], arg['<z_i>']]))
+    imgsize = int(arg['--imgsize'])
+    center = complex(*map(float, arg['--center'].split(',')))
+    zoom = float(arg['--zoom'])
+    zLimit = float(arg['--zlimit'])
+    cmap = get_cmap(arg['--cmap'])
+
+    grid = np.ndarray(shape=(imgsize, imgsize), dtype=np.uint8)
 
     longestIter = 0.0
 
     for (j, i), v in np.ndenumerate(grid):
         pz = pixelCoord(i,j)
         finalMag, numIter = iterate(pz)
-#        smoothedIter = numIter + 1.0 + log(log(zLimit)/log(finalMag))/log(2.0)
         smoothedIter = numIter
+        if arg['--soften']:
+            smoothedIter += 1.0 + log(log(zLimit)/log(finalMag))/log(2.0)
         if smoothedIter > longestIter:
             longestIter = smoothedIter
         grid[i, j] = smoothedIter
 
     print('Longest iteration: %d' % longestIter)
 
-    image = np.ndarray(shape=(yPixels, xPixels, 3), dtype=np.uint8)
+    image = np.ndarray(shape=(imgsize, imgsize, 3), dtype=np.uint8)
 
     for (i, j), v in np.ndenumerate(grid):
-        image[i, j] = colorize(v, longestIter)
+        image[i, j] = colorize(v, longestIter, cmap)
 
     img = Image.fromarray(image, 'RGB')
-    img.save('julia.png')
+    img.save(arg['--imgname'])
 
